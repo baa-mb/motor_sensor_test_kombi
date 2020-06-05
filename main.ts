9d47eee55aefa0ef5525f7b3aff7e89d71d8d5cb
baa <34665120+baa-mb@users.noreply.github.com>
2020-06-05 21:02:25 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,21 @@
+let distanz = 0
+basic.showIcon(IconNames.Yes)
+let richtung = false
+let richtung_alt = false
+basic.forever(function () {
+    distanz = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    richtung = distanz > 5
+    if (richtung != richtung_alt) {
+        if (richtung) {
+            pins.servoWritePin(AnalogPin.P0, 0)
+        } else {
+            pins.servoWritePin(AnalogPin.P0, 180)
+        }
+        richtung_alt = richtung
+    }
+    basic.showNumber(distanz)
+})
